50f3ec1737b76910380d9e951c9a5e07d5bf4b82
jSdCool <37940266+jSdCool@users.noreply.github.com>
2021-10-28 17:40:24 -0400

    added example op modes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleIterativeOpMode.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="NAME HERE", group="Iterative Opmode")//register this op mode in the op mode list on the phone
+@Disabled//don't include this in your code
+public class ExampleIterativeOpMode extends OpMode {
+
+
+    @Override
+    public void init() {//Code to run ONCE when the driver hits INIT
+
+    }
+
+    @Override
+    public void init_loop() {//Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+
+    }
+
+    @Override
+    public void start() {//Code to run ONCE when the driver hits PLAY
+
+    }
+
+    @Override
+    public void loop() {//Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+
+    }
+
+    @Override
+    public void stop() {//Code to run ONCE after the driver hits STOP
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleLinerOpMode.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="op mode name here", group="Linear Opmode")//register this op mode in the op mode list on the phone
+@Disabled//don't include this line in your code
+public class ExampleLinerOpMode extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+
+
+        waitForStart();//wait for the play button to be pressed
+
+        while (opModeIsActive()) {//loop this until stop button is pressed
+
+        }
+    }
+}
