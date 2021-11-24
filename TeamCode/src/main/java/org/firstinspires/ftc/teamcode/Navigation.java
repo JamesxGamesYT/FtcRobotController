b3c020be92881ce8ffb3653898de6a8a434c3e3d
lol-cubes <arinmkhare@gmail.com>
2021-11-24 11:07:25 -0500

    Add constructor, rename methods, incorporate motors and positioning for Navigation class.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Navigation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Navigation.java
@@ -1,20 +1,33 @@
-/*
-* Authors: ningning ying, elicia 0
-* https://code-with-me.jetbrains.com/XKKH3G59srClhs2J-yw3zQ#p=IC&fp=11E83DBF4852D367814E3D348B3543B60C28995641E977BD9AC2E26485A08445
-* */
+/* Authors: Ningning Ying, Elicia Esmeris, Smyan Sengupta, Cristian Santibanez, Arin Khare, Kristal Lin
+ */
+
+package org.firstinspires.ftc.teamcode;
+
 
 import java.util.ArrayList;
-import Point;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.teamcode.Point;
+import org.firstinspires.ftc.teamcode.CVPositioning;
+import org.firstinspires.ftc.teamcode.EncoderPositioning;
 
 
-/** Keeps track of robot's desired path and makes it follow it accurately.
+/** Keeps track of the robot's desired path and makes it follow it accurately.
  */
 public class Navigation
 {
-    // First point in this arraylist is the first point that robot is planning to go to.
+    final double SPEED = 1.0;
+
+    // First point in this ArrayList is the first point that robot is planning to go to.
     // This condition must be maintained (points should be deleted as the robot travels)
-    private ArrayList<Point> path = new ArrayList<Point>();
-    private double speed;
+    private ArrayList<Point> path = new ArrayList<>();
+
+    private DcMotor frontRight, rearRight, frontLeft, rearLeft;
+    private CVPositioning cvPositioning;
+    private EncoderPositioning encoderPositioning;
+
+    public Navigation(DcMotor frontRight, DcMotor rearRight, DcMotor frontLeft, DcMotor rearLeft,
+                      CVPositioning cvPositioning, EncoderPositioning encoderPositioning) {}
 
     /** Adds a desired point to the path.
      */
@@ -24,24 +37,19 @@ public class Navigation
      */
     public void addDestination(Point loc, int index) {}
 
-    /** Travel a certain number of points along the path.
+    /** Makes the robot travel a certain number of points along the path.
      *  @param numPoints The number of points along the path to travel.
      */
     public void travel(int numPoints) {}
 
-    /** Moves the robot in a straight line for a certain distance.
+    /** Makes the robot travel in a straight line for a certain distance.
      *  @param dist The distance the robot should travel.
      *  @param relativeAngle The relative angle from the current position of the robot
      */
-    private void moveLinear(double dist, double relativeAngle) {};
+    private void travelLinear(double dist, double relativeAngle) {}
 
-    /** Determine the angle relative to the angle the robot is facing that it needs to go in in order to get to the
-     *  next point. For example, this should be used to determine a value to be passed into moveLinear as relativeAngle.
+    /** Determines the amount of rotation required for the robot to face its next destination. For example, this should
+     *  be used to determine a value to be passed into moveLinear as relativeAngle.
      */
-     private double getAngleToNextPoint() {}
+    private double getAngleToNextPoint() { return 0.0; }
 }
-
-
-
-
-
