a228314f154df1d35f5aef9436ae8882c721d208
6460 <6460@gmail.com>
2021-11-29 17:24:07 -0500

    Today, we found the position of the robot at any time by using the power values of each motor.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PositionManager.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+public class PositionManager {
+    private static double xcor;
+    private static double ycor;
+    private static double rotation;
+    PositionManager(){
+        xcor = 0;
+        ycor = 0;
+        rotation = 0;
+    }
+    PositionManager(double x, double y, double r){
+        xcor = x;
+        ycor = y;
+        rotation = r;
+    }
+    public static double getXcor(){
+        return(xcor);
+    }
+    public static double getYcor(){
+        return(ycor);
+    }
+    public static double getRotation(){
+        return(rotation);
+    }
+    public static void setXcor(double x){
+        xcor = x;
+    }
+    public static double setYcor(double y){
+        ycor = y;
+    }
+    public static double setRotation(double r){
+        rotation = r;
+    }
+}
