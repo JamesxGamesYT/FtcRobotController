215ebe0e7e93b8a090f95099d0e8ca6464bb55a2
lol-cubes <arinmkhare@gmail.com>
2021-12-01 21:19:10 -0500

    Implement Robot and RobotConfig classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -7,6 +7,9 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 /** A finite state of the robot: keeps track of its state (motors and position) and what actions it is doing (enums)
  */
@@ -25,24 +28,50 @@ public class Robot {
     public Servo claw;
 
     // Positioning
-    public PositionManager position;
+    public PositionManager positionManager;
 
-    public Robot(HardwareMap hardwareMap) {}
+    public Robot(HardwareMap hardwareMap) {
+        // Initialize states.
+        carouselMotorState = CarouselMotorState.CHECK_START;
+        slidesMotorsState = SlidesMotorsState.CHECK_EXTEND;
+        clawMotorState = ClawMotorState.CHECK_CLOSE;
+
+        // Initialize hardware.
+        carousel = hardwareMap.get(DcMotor.class, RobotConfig.MotorNames.get(RobotConfig.Motors.CAROUSEL));
+        slidesLeft = hardwareMap.get(DcMotor.class, RobotConfig.MotorNames.get(RobotConfig.Motors.SLIDES_LEFT));
+        slidesRight = hardwareMap.get(DcMotor.class, RobotConfig.MotorNames.get(RobotConfig.Motors.SLIDES_RIGHT));
+        frontRightDrive = hardwareMap.get(DcMotor.class, RobotConfig.MotorNames.get(RobotConfig.Motors.FRONT_RIGHT_DRIVE));
+        rearRightDrive = hardwareMap.get(DcMotor.class, RobotConfig.MotorNames.get(RobotConfig.Motors.REAR_RIGHT_DRIVE));
+        frontLeftDrive = hardwareMap.get(DcMotor.class, RobotConfig.MotorNames.get(RobotConfig.Motors.FRONT_LEFT_DRIVE));
+        rearLeftDrive = hardwareMap.get(DcMotor.class, RobotConfig.MotorNames.get(RobotConfig.Motors.REAR_LEFT_DRIVE));
+        claw = hardwareMap.get(Servo.class, RobotConfig.ServoNames.get(RobotConfig.Servos.CLAW));
+
+        frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rearLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        frontRightDrive.setDirection(DcMotor.Direction.REVERSE);
+        rearRightDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        // Initialize position manager.
+        positionManager = new PositionManager();
+    }
 }
 
 
-// TODO: Decide on whether or not we need this class.
-//class RobotConfig {
-//    enum Motors {CAROUSEL, SLIDES_LEFT, SLIDES_RIGHT, REAR_LEFT_DRIVE,
-//                 REAR_RIGHT_DRIVE, FRONT_LEFT_DRIVE, FRONT_RIGHT_DRIVE}
-//
-//    public static Map<Motors, String> MotorNames = new HashMap<Motors, String>() {{
-//        put(Motors.CAROUSEL, "carousel");
-//        put(Motors.SLIDES_LEFT, "slides_left");
-//        put(Motors.SLIDES_RIGHT, "slides_right");
-//        put(Motors.REAR_LEFT_DRIVE, "rear_left");
-//        put(Motors.REAR_RIGHT_DRIVE, "rear_right");
-//        put(Motors.FRONT_LEFT_DRIVE, "front_left");
-//        put(Motors.FRONT_RIGHT_DRIVE, "front_right");
-//    }};
-//}
+class RobotConfig {
+    enum Motors {CAROUSEL, SLIDES_LEFT, SLIDES_RIGHT, REAR_LEFT_DRIVE,
+                 REAR_RIGHT_DRIVE, FRONT_LEFT_DRIVE, FRONT_RIGHT_DRIVE}
+
+    enum Servos {CLAW}
+
+    public static Map<Motors, String> MotorNames = new HashMap<Motors, String>() {{
+        put(Motors.CAROUSEL, "carousel");
+        put(Motors.SLIDES_LEFT, "slides_left");
+        put(Motors.SLIDES_RIGHT, "slides_right");
+        put(Motors.REAR_LEFT_DRIVE, "rear_left");
+        put(Motors.REAR_RIGHT_DRIVE, "rear_right");
+        put(Motors.FRONT_LEFT_DRIVE, "front_left");
+        put(Motors.FRONT_RIGHT_DRIVE, "front_right");
+    }};
+
+    public static Map<Servos, String> ServoNames = new HashMap<Servos, String>() {{ put(Servos.CLAW, "claw"); }};
+}
