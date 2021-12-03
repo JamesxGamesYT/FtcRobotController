66a734212d997aa4bdb7d892db67016d375b6dec
Arin Khare <51566200+lol-cubes@users.noreply.github.com>
2021-12-03 16:06:03 -0500

    Create method outline and resetEncoders method.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderPositioning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderPositioning.java
@@ -8,7 +8,12 @@ public class EncoderPositioning {
     /** Checks the robot's encoders to get an estimate of the distance and direction traveled.
      *  @return a position in the form of a vector from the origin that can be added to an existing measurement
      */
-    public Position updateDeltaEstimate(Robot robot) {return null;}
+    public Position updateDeltaEstimate(Robot robot) {
+        // Read encoder values and use them to create a Position that represents the robot's movement relative to the last time the encoders were read.
+        // Reset encoder values to zero.
+        // call submitEstimate        
+        return null;
+    }
 
 
     /** Updates the encoder's position estimate in the robot's PositionManager
@@ -16,4 +21,9 @@ public class EncoderPositioning {
     private void submitEstimate(Robot robot, Position delta) {
         robot.position.updateEncoderPosition(delta);
     }
+    
+    
+    /** Resets the encoder values to zero.
+     */
+    private void resetEncoders(Robot robot) {}
 }
