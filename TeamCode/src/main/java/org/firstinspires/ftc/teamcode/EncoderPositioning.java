400428f47f81324648744c0df0cb17bd80edb6ec
6460 <6460@gmail.com>
2021-12-22 16:30:57 -0500

    Implement encoder math that utilizes encoder data to distance conversion factors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderPositioning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderPositioning.java
@@ -6,8 +6,9 @@ import java.util.HashMap;
  *  position to add onto.
  */
 public class EncoderPositioning {
-    static int COUNTS_PER_ROTATION = 280;
+    static int ENCODER_COUNTS_PER_ROTATION = 280;
     static int MAGICAL_FACTOR = 1;
+    static double MAGICAL_RATIO = MAGICAL_FACTOR / ENCODER_COUNTS_PER_ROTATION;
 
     static HashMap<Robot.WheelConfiguration, Double> RollerAngles = new HashMap<Robot.WheelConfiguration, Double> (){{
         put(Robot.WheelConfiguration.FRONTRIGHT, Math.PI / 4.d);
@@ -18,38 +19,40 @@ public class EncoderPositioning {
 
 
     /** Checks the robot's encoders to get an estimate of the distance and direction traveled.
+     *  Read encoder values and use them to create a Position that represents the robot's movement relative to the last time the encoders were read.
+     *  Reset encoder values to zero.
+     *  Call submitEstimate
      *  @return a position in the form of a vector from the origin that can be added to an existing measurement
      */
     public void updateDeltaEstimate(Robot robot) {
-        // Read encoder values and use them to create a Position that represents the robot's movement relative to the last time the encoders were read.
-        // Reset encoder values to zero.
-        // call submitEstimate
-        // 280 encoder counts per revolution
+        double theta = robot.positionManager.position.getRotation();
+        double deltaPSumX = 0.0d, deltaPSumY = 0.0;;
 
+        for (HashMap.Entry<Robot.WheelConfiguration, Double> rollerAngle : RollerAngles.entrySet()) {
+            int encoderCounts = robot.driveMotors.get(rollerAngle.getKey()).getCurrentPosition();
+            double force = rollerAngle.getValue();
+
+            deltaPSumX += (encoderCounts * ((Math.sin(theta) * Math.sin(force)) + (Math.cos(theta) * Math.cos(force)))) / 2.0;
+            deltaPSumY += (encoderCounts * ((Math.sin(theta) * Math.cos(force)) + (Math.cos(theta) * Math.sin(force)))) / 2.0;
+        }
+
+        submitEstimate(robot, new Position(MAGICAL_RATIO * (deltaPSumX), MAGICAL_RATIO * (deltaPSumY), 0.0));
+
+
+//        double ADEncCount = robot.frontLeftDrive.getCurrentPosition() + robot.rearRightDrive.getCurrentPosition();
+//        double BCEncCount = robot.rearLeftDrive.getCurrentPosition() + robot.frontRightDrive.getCurrentPosition();
 //
-//        double frontLeftDelta = 0, frontRightDelta = 0, rearRightDelta = 0, rearLeftDelta = 0;
+//        double ADangle = Math.acos((2 * ADEncCount + Math.sqrt(8 - 4 * (ADEncCount * ADEncCount))) / 4);
+//        double BCangle = Math.acos((2 * BCEncCount + Math.sqrt(8 - 4 * (BCEncCount * BCEncCount))) / 4);
 //
-//        for (HashMap.Entry<Robot.WheelConfiguration, Double> rollerAngle : RollerAngles.entrySet())
-//        {
+//        double secondXcor = BCEncCount * Math.cos(BCangle);
+//        double secondYcor = BCEncCount * Math.sin(BCangle);
 //
-//        }
+//        double thirdXcor = secondXcor - ADEncCount * Math.cos(ADangle);
+//        double thirdYcor = secondYcor + ADEncCount * Math.sin(ADangle);
+//        double orientation = Math.atan(thirdXcor / thirdYcor);
 
-
-
-        double ADEncCount = robot.frontLeftDrive.getCurrentPosition() + robot.rearRightDrive.getCurrentPosition();
-        double BCEncCount = robot.rearLeftDrive.getCurrentPosition() + robot.frontRightDrive.getCurrentPosition();
-
-        double ADangle = Math.acos((2 * ADEncCount + Math.sqrt(8 - 4 * (ADEncCount * ADEncCount))) / 4);
-        double BCangle = Math.acos((2 * BCEncCount + Math.sqrt(8 - 4 * (BCEncCount * BCEncCount))) / 4);
-
-        double secondXcor = BCEncCount * Math.cos(BCangle);
-        double secondYcor = BCEncCount * Math.sin(BCangle);
-
-        double thirdXcor = secondXcor - ADEncCount * Math.cos(ADangle);
-        double thirdYcor = secondYcor + ADEncCount * Math.sin(ADangle);
-        double orientation = Math.atan(thirdXcor / thirdYcor);
-
-        submitEstimate(robot, new Position(thirdXcor, thirdYcor, 0.0));
+//        submitEstimate(robot, new Position(thirdXcor, thirdYcor, 0.0));
         resetEncoders(robot);
     }
 
