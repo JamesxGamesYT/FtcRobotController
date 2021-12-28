fb0d8d1fa55fe79389ea47224680e10ee55f2833
k-vernooy <kai.a.vernooy@gmail.com>
2021-12-28 11:19:39 -0500

    Fully implement calibration, implement homography calculation and ORB matching for positioning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ComputerVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ComputerVision.java
@@ -1,25 +1,28 @@
 package org.firstinspires.ftc.teamcode;
 
-import androidx.annotation.Nullable;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import androidx.annotation.Nullable;
+import android.os.Environment;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.opencv.android.Utils;
+import android.graphics.BitmapFactory;
+import android.graphics.Bitmap;
+
 import org.opencv.core.*;
 
+import org.opencv.features2d.*;
 import org.opencv.imgproc.Imgproc;
-import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.calib3d.Calib3d;
-import org.opencv.features2d.Features2d;
-
-import org.opencv.features2d.FlannBasedMatcher;
-import org.opencv.features2d.SIFT;
 
 
 import org.openftc.easyopencv.*;
 
+//Imageio.read()
 
 /** Managing class for opening cameras, attaching pipelines, and beginning streaming.
  */
@@ -53,17 +56,17 @@ public class ComputerVision {
 }
 
 
-/** Contains all image processing done for scanning the barcode and getting position
+/** Contains all image processing done for scanning the barcode and getting position.
+ * This will
  */
-class CVOpModePipeline extends OpenCvPipeline {
+class AutonPipeline extends OpenCvPipeline {
 
     final private Robot robot;
+    final private Telemetry telemetry;
+    final private Mat output;  // Frame to be displayed on the phone
 
-    // Holds the frame to be displayed on the phone
-    final private Mat output;
 
-
-    CVOpModePipeline(Robot robot) {
+    AutonPipeline(Robot robot, Telemetry telemetry) {
         super();
         this.robot = robot;
 
@@ -83,29 +86,33 @@ class CVOpModePipeline extends OpenCvPipeline {
         barcodeTapeRegionsBlue = new Mat();
         barcodeTapeRegionsRed1 = new Mat();
         barcodeTapeRegionsRed2 = new Mat();
+
+        this.telemetry = telemetry;
     }
 
 
-    /** The main pipeline method, called whenever a frame is received
-     * @param input The current frame read from the attached camera.
-     *              NOTE: the camera will be mounted in landscape, so make sure to flip x/y coords
-     * @return An output frame to be displayed on the phone
+    /** The main pipeline method, called whenever a frame is received. It will execute all necessary CV tasks, such as localization and barcode scanning
+     *  @param input The current frame read from the attached camera.
+     *               NOTE: the camera will be mounted in landscape, so make sure to flip x/y coords
+     *  @return An output frame to be displayed on the phone
      */
     @Override
     public Mat processFrame(Mat input) {
-//        if (robot.barcodeScanState == Robot.BarcodeScanState.SCAN) {
-//            int result = processBarcodeFrame(input);
-//            robot.numBarcodeAttempts++;
-//
-//            if (robot.numBarcodeAttempts >= Robot.MaxBarcodeAttempts || result != -1) {
-//                robot.barcodeScanResult = result;
-//                robot.barcodeScanState = Robot.BarcodeScanState.CHECK_SCAN;
-//            }
-//        }
+        // Check if a barcode scan has been requested
+        if (robot.barcodeScanState == Robot.BarcodeScanState.SCAN) {
+            int result = processBarcodeFrame(input);
+            robot.numBarcodeAttempts++;
+
+            if (robot.numBarcodeAttempts >= Robot.MaxBarcodeAttempts || result != -1) {
+                robot.barcodeScanResult = result; // result may still be -1, meaning no result
+                robot.barcodeScanState = Robot.BarcodeScanState.CHECK_SCAN;
+            }
+            else return input;  // We have more iterations of barcode scanning to do, so don't waste time on positioning
+        }
 
         Position currentPosition = processPositioningFrame(input, output);
         if (currentPosition != null) robot.positionManager.updateCvPosition(currentPosition);
-//        input.copyTo(output);
+
         return output;
     }
 
@@ -113,15 +120,21 @@ class CVOpModePipeline extends OpenCvPipeline {
 
     // CV POSITIONING
     // =================
-    private static final SIFT sift = SIFT.create();
+    private static final SIFT Sift = SIFT.create();
+    private static final ORB Orb = ORB.create();
+    private static final BFMatcher BfMatcher = BFMatcher.create(BFMatcher.BRUTEFORCE_HAMMING, false);
+
+
     private static final MatOfKeyPoint kp1 = new MatOfKeyPoint(), kp2 = new MatOfKeyPoint();
     private static final Mat des1 = new Mat(), des2 = new Mat();
 
+//    enum NavTargets {ALLIANCE}
+
     static List<Point3> NavTargetsWorldSpace = new ArrayList<Point3> () {{
-        add(new Point3(0,42 + (5.5),5.75 - (8.5 / 2)));
-        add(new Point3(0,42 - (5.5),5.75 - (8.5 / 2)));
-        add(new Point3(0,42 - (5.5),5.75 + (8.5 / 2)));
-        add(new Point3(0,42 + (5.5),5.75 + (8.5 / 2)));
+        add(new Point3(0,42 + (5.5),5.75 - (8.5 / 2))); // br
+        add(new Point3(0,42 - (5.5),5.75 - (8.5 / 2))); // bl
+        add(new Point3(0,42 - (5.5),5.75 + (8.5 / 2))); // tl
+        add(new Point3(0,42 + (5.5),5.75 + (8.5 / 2))); // tr
     }};
 
 
@@ -133,70 +146,120 @@ class CVOpModePipeline extends OpenCvPipeline {
      * @param output An image to draw the detected KeyPoint matches to
      * @return The input {@param points}, transformed into the destination space
      */
-    private static MatOfPoint2f DetectTemplate(Mat template, Mat frame, /*MatOfPoint2f points,*/ Mat output) {
-        sift.detectAndCompute(template, null, kp1, des1);
-        sift.detectAndCompute(frame, null, kp2, des2);
+    @Nullable
+    private static MatOfPoint2f DetectTemplate(Mat template, Mat frame, MatOfPoint2f points, Mat output) {
+        Orb.detectAndCompute(template, new Mat(), kp1, des1);
+        Orb.detectAndCompute(frame, new Mat(), kp2, des2);
 
         // find a way to set knn params here
-        FlannBasedMatcher matcher = FlannBasedMatcher.create();
-
-        ArrayList<MatOfDMatch> matches = new ArrayList<MatOfDMatch>();
+//        FlannBasedMatcher matcher = FlannBasedMatcher.create();
+        MatOfDMatch matchesB = new MatOfDMatch();
+//        ArrayList<MatOfDMatch> matches = new ArrayList<MatOfDMatch>();
         ArrayList<DMatch> goodMatches = new ArrayList<DMatch>();
 
-        matcher.knnMatch(des1, des2, matches, 2);
 
-        // check total(), might not be correct
+        if (des1.empty() || des2.empty()) return null;
         if (kp1.total() < 2 && kp2.total() < 2) return null;
 
-        matcher.knnMatch(des1, des2, matches, 2);
+        des1.convertTo(des1, CvType.CV_32F);
+        des2.convertTo(des2, CvType.CV_32F);
+//        matcher.knnMatch(des1, des2, matches, 2);
 
+//        BfMatcher.knnMatch(des1, des2, matches, 2);
+        BfMatcher.match(des1, des2, matchesB);
 
-        for (MatOfDMatch matchSet : matches) {
-            DMatch[] matchSetArr = matchSet.toArray();
-            if (matchSetArr.length < 2)
-                continue;
-
-            DMatch m1 = matchSetArr[0];
-            DMatch m2 = matchSetArr[1];
-
-            if (m1.distance < 0.7 * m2.distance)
-                goodMatches.add(m1);
-
-//            if (m2.distance - m1.distance > 0.19)
-//                goodMatches.add(m1);
+        for (int i = 0; i < des1.rows(); i++) {
+            if ((matchesB.toList().get(i)).distance < 200) goodMatches.add(matchesB.toList().get(i));
         }
-
+//
+//        for (MatOfDMatch matchSet : matches) {
+//            DMatch[] matchSetArr = matchSet.toArray();
+//            if (matchSetArr.length < 2)
+//                continue;
+//
+//            DMatch m1 = matchSetArr[0];
+//            DMatch m2 = matchSetArr[1];
+//
+//            if (m1.distance < 0.7 * m2.distance)
+//                goodMatches.add(m1);
+//
+////            if (m2.distance - m1.distance > 0.19)
+////                goodMatches.add(m1);
+//        }
+//
 
         MatOfDMatch goodMatchesMat = new MatOfDMatch();
         goodMatchesMat.fromList(goodMatches);
-
         Features2d.drawMatches(template, kp1, frame, kp2, goodMatchesMat, output);
-        return null;
+        Imgproc.resize(output, output, frame.size(), 0, 0, Imgproc.INTER_CUBIC);
+
+        if (goodMatches.size() < 10) return null;
+
+        ArrayList<org.opencv.core.Point> obj = new ArrayList<org.opencv.core.Point>();
+        ArrayList<org.opencv.core.Point> scene = new ArrayList<org.opencv.core.Point>();
+
+        for (DMatch match : goodMatches) {
+            obj.add(kp1.toList().get(match.queryIdx).pt);
+            scene.add(kp2.toList().get(match.trainIdx).pt);
+        }
+
+        MatOfPoint2f objM = new MatOfPoint2f(), sceneM = new MatOfPoint2f();
+        objM.fromList(obj);
+        sceneM.fromList(scene);
+
+//        Mat homography = Calib3d.findHomography(objM, sceneM, Calib3d.RANSAC, 5.0, new Mat());
+        Mat homography = Calib3d.findHomography(objM, sceneM, Calib3d.LMEDS);
+
+        MatOfPoint2f result = new MatOfPoint2f();
+        Core.perspectiveTransform(points, result, homography);
+
+        // homography LMeDS
+        return result;
     }
 
 
-    /** Main CV localization estimator - determines position relative to any detected navigation targets
+    Mat template1 = new Mat();
+
+    /** Main CV localization estimator - determines position relative to any detected navigation targets2
      * @param input The frame to be processed (either containing nav targets or not)
      * //@param output
-     * @return Determined position of the robot, as presented in the {@param input} image.
+     * @return Determined pos("Stuff", frame.at(Point3.class, 0, 0).getV());
+        telemetry.update(ition of the robot, as presented in the {@param input} image.
      */
-    @Nullable Position processPositioningFrame(Mat input, Mat output) {
-//        Mat frame = Imgcodecs.imread("filename", 0);
-//        frame.copyTo(output);
+    @Nullable
+    Position processPositioningFrame(Mat input, Mat output) {
+        Bitmap bitmap = BitmapFactory.decodeFile(Environment.getExternalStorageDirectory().getAbsolutePath() + "/FIRST/navimgs/features3.png");
+        Utils.bitmapToMat(bitmap, template1);
 
-//        Imgproc.cvtColor(input, output, Imgproc.COLOR_RGB2HSV);
-//        MatOfPoint3f[]
-//        Calib3d.solvePnP()
-//
-//        input.copyTo(output);
+        input.copyTo(output);
+
+        Imgproc.cvtColor(template1, template1, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.cvtColor(input, input, Imgproc.COLOR_BGR2GRAY);
+
+        List<org.opencv.core.Point> templateCorners = new ArrayList<org.opencv.core.Point> () {{
+            add(new org.opencv.core.Point(template1.cols(), template1.rows()));     // br
+            add(new org.opencv.core.Point(0, template1.rows()));                 // bl
+            add(new org.opencv.core.Point(0, 0));                             // tl
+            add(new org.opencv.core.Point(template1.cols(), 0));                 // tr
+        }};
+
+        MatOfPoint2f points = new MatOfPoint2f();
+        points.fromList(templateCorners);
+
+        MatOfPoint2f screenPoints = DetectTemplate(template1, input, points, output);
+
+        if (screenPoints == null) return null;
+        Imgproc.line(output, screenPoints.toArray()[0], screenPoints.toArray()[1], new Scalar(0, 255, 0), 4);
+        Imgproc.line(output, screenPoints.toArray()[1], screenPoints.toArray()[2], new Scalar(0, 0, 255), 4);
+        Imgproc.line(output, screenPoints.toArray()[2], screenPoints.toArray()[3], new Scalar(255, 0, 0), 4);
+        Imgproc.line(output, screenPoints.toArray()[3], screenPoints.toArray()[0], new Scalar(0, 255, 255), 4);
         return null;
+//        Calib3d.solvePnP(NavTargetsWorldSpace, screenPoints, )
     }
-
 
 
     // BARCODE SCANNING
     // =================
-
 
     // Single-time allocated mats that will hold frame processing data
     final private Mat barcodeHsv;
@@ -298,50 +361,117 @@ class CVOpModePipeline extends OpenCvPipeline {
 }
 
 
+
+
+
+/** Pipeline to perform intrinsic camera calibration with user-defined snapshots
+ *
+ */
 class CalibrationPipeline extends OpenCvPipeline {
 
     final private Mat output;
+
+    boolean captureRequested;
     boolean doCapture;
+    boolean displayCapture;
+    int displayCaptureFrames = 0;
+    static final int MaxDisplayCaptureDelay = 20;
+    static final int NumTotalCaptures = 3;
+
+    final static Size BoardSize = new Size(10, 7);
+    final static double boardSquareSize = 1.0;  // TODO: measure this
+    static MatOfPoint3f CheckerboardWorldCoords = new MatOfPoint3f();
+
+
+    final List<Mat> imgCorners = new ArrayList<Mat>();  // 2d coordinates of checkerboard corners for each frame
+    final List<Mat> objCorners = new ArrayList<Mat>();  // 3d corners of the checkerboard (each element should be identical)
+
+    final Mat lastCapture = new Mat();
+
+
 
     CalibrationPipeline() {
         super();
         output = new Mat();
         doCapture = false;
+        displayCapture = false;
+
+        List<Point3> checkerboardWorldCoordsList = new ArrayList<>();
+        for (int i = 0; i < BoardSize.height; i++) {
+            for (int j = 0; j < BoardSize.width; j++) {
+                checkerboardWorldCoordsList.add(new Point3((double)j * boardSquareSize, (double)i * boardSquareSize, 0));
+            }
+        }
+
+        CheckerboardWorldCoords.fromList(checkerboardWorldCoordsList);
     }
 
+
+    public Mat cameraMatrix = new Mat();
+    public Mat distortionMatrix = new Mat();
+    public boolean calibrated = false;
 
     @Override
     public Mat processFrame(Mat input) {
-        input.copyTo(output);
 
-        if (doCapture) {
-            calibrate(input, output);
-            doCapture = false;
+        // Check if we've got enough calibration frames
+        if (imgCorners.size() >= NumTotalCaptures && !calibrated) {
+            Calib3d.calibrateCamera(objCorners, imgCorners, input.size(), cameraMatrix, distortionMatrix, new ArrayList<Mat>(), new ArrayList<Mat>());
+            calibrated = true;
+            return input;
         }
 
-        return output;
+        if (doCapture) {
+            input.copyTo(lastCapture);
+            MatOfPoint2f imgP = Calibrate(input, lastCapture);
+            if (imgP != null) {
+                imgCorners.add(imgP);
+                objCorners.add(CheckerboardWorldCoords);
+                displayCapture = true;
+            }
+
+            doCapture = false;
+            captureRequested = false;
+        }
+
+        if (captureRequested) {
+            doCapture = true;
+            Imgproc.putText(input, "Detecting...", new org.opencv.core.Point(10.0, 50.0), Imgproc.FONT_HERSHEY_COMPLEX, 1, new Scalar(0, 0, 0), 4);                               // Thickness
+        }
+
+        if (displayCapture) {
+            displayCaptureFrames++;
+
+            if (displayCaptureFrames >= MaxDisplayCaptureDelay) {
+                displayCapture = false;
+                displayCaptureFrames = 0;
+            }
+
+            return lastCapture;
+        }
+
+        return input;
     }
 
 
-    void calibrate(Mat input, Mat output) {
+    @Nullable
+    static MatOfPoint2f Calibrate(Mat input, Mat output) {
         Imgproc.cvtColor(input, input, Imgproc.COLOR_RGB2GRAY);
-
         MatOfPoint2f corners = new MatOfPoint2f();
-        Size boardSize = new Size(10, 7);
 
-        boolean found = Calib3d.findChessboardCorners(input, boardSize, corners, Calib3d.CALIB_CB_ADAPTIVE_THRESH | Calib3d.CALIB_CB_FILTER_QUADS);
+        boolean found = Calib3d.findChessboardCorners(input, BoardSize, corners, Calib3d.CALIB_CB_ADAPTIVE_THRESH | Calib3d.CALIB_CB_FILTER_QUADS);
 
-        if (found) {
-            Calib3d.find4QuadCornerSubpix(input, corners, new Size(10, 10));
-            Calib3d.drawChessboardCorners(output, boardSize, corners, found);
-        }
+        if (!found) return null;
+
+        Calib3d.find4QuadCornerSubpix(input, corners, new Size(10, 10));
+        Calib3d.drawChessboardCorners(output, BoardSize, corners, true);
+        return corners;
     }
 
 
     @Override
     public void onViewportTapped() {
-        doCapture = true;
-//        camera.pauseViewport();
+        if (!calibrated) captureRequested = true;
     }
 }
 
